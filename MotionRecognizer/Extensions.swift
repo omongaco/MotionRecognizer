e7e08c7097fb1ad256711f5aa7261ac050889624
ansyarhafid <ansyar@dotugo.com>
2017-10-20 00:23:25 +0700

    Anyyong

--- MotionRecognizer/Extensions.swift
+++ MotionRecognizer/Extensions.swift
@@ -1,3 +1,4 @@
+
 //
 //  Extensions.swift
 //  MotionRecognizer
@@ -7,3 +8,15 @@
 //
 
 import Foundation
+import UIKit
+
+extension CGRect {
+    func scaled(to size: CGSize) -> CGRect {
+        return CGRect(
+            x: self.origin.x * size.width,
+            y: self.origin.y * size.height,
+            width: self.size.width * size.width,
+            height: self.size.height * size.height
+        )
+    }
+}
--- MotionRecognizer/ViewController.swift
+++ MotionRecognizer/ViewController.swift
@@ -6,122 +6,220 @@
 //  Copyright © 2017 Ansyar Hafid. All rights reserved.
 //
 
+import UIKit
 import AVFoundation
 import Vision
-import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    var session                         : AVCaptureSession?
     
-    @IBOutlet private weak var cameraView: UIView!
+    let shapeLayer                      = CAShapeLayer()
+    let faceDetection                   = VNDetectFaceRectanglesRequest()
+    let faceLandmarks                   = VNDetectFaceLandmarksRequest()
+    let faceLandmarksDetectionRequest   = VNSequenceRequestHandler()
+    let faceDetectionRequest            = VNSequenceRequestHandler()
     
-    @IBOutlet private weak var highlightView: UIView? {
-        didSet{
-            self.highlightView?.layer.borderColor = UIColor.red.cgColor
-            self.highlightView?.layer.borderWidth = 4
-            self.highlightView?.backgroundColor = .clear
-        }
-    }
-    
-    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-    private lazy var captureSession: AVCaptureSession = {
-        let session             = AVCaptureSession()
-        session.sessionPreset   = AVCaptureSession.Preset.photo
-        guard
-            let frontCamera     = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: AVMediaType.video, position: AVCaptureDevice.Position.back),
-            let input           = try? AVCaptureDeviceInput(device: frontCamera)
-        else { return session }
+    lazy var previewLayer: AVCaptureVideoPreviewLayer? = {
+        guard let session = self.session else { return nil }
         
-        session.addInput(input)
+        var previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = .resizeAspectFill
         
-        return session
+        return previewLayer
     }()
     
-    private let visionSequenceHandler = VNSequenceRequestHandler()
-    private var lastObservation: VNDetectedObjectObservation?
-
+    var frontCamera: AVCaptureDevice? = {
+        return AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: AVMediaType.video, position: .front)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        //make the camera appear on the screen
-        self.cameraView.layer.addSublayer(self.cameraLayer)
-        
-        //register for delegation
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
-        self.captureSession.addOutput(videoOutput)
-        
-        //begin session
-        self.captureSession.startRunning()
+        sessionPrepare()
+        session?.startRunning()
     }
-
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        self.cameraLayer.frame = self.cameraView?.bounds ?? .zero
+        previewLayer?.frame = view.frame
+        shapeLayer.frame    = view.frame
     }
     
-    private func handleVisionRequestUpdate(_ request: VNRequest, error: Error?) {
-        DispatchQueue.main.async {
-            //make sure there is a result
-            guard let newObservation    = request.results?.first as? VNDetectedObjectObservation else { return }
-            self.lastObservation        = newObservation
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard let previewLayer = previewLayer else { return }
+        
+        view.layer.addSublayer(previewLayer)
+        
+        shapeLayer.strokeColor  = UIColor.green.cgColor
+        shapeLayer.lineWidth    = 2.0
+        
+        //needs to filp coordinate system for Vision
+        shapeLayer.setAffineTransform(CGAffineTransform(scaleX: -1, y: -1))
+        
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    func sessionPrepare() {
+        session             = AVCaptureSession()
+        guard let session   = session, let captureDevice = frontCamera else { return }
+        
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            session.beginConfiguration()
             
-            // check the confidence level before updating the UI
-            guard newObservation.confidence >= 0.3 else {
-                // hide the rectangle when we lose accuracy so the user knows something is wrong
-                self.highlightView?.frame = .zero
-                return
+            if session.canAddInput(deviceInput) {
+                session.addInput(deviceInput)
             }
             
-            //calculate view rect
-            var transformedRect         = newObservation.boundingBox
-            transformedRect.origin.y    = 1 - transformedRect.origin.y
-            let convertedRect           = self.cameraLayer.layerRectConverted(fromMetadataOutputRect: transformedRect)
+            let output = AVCaptureVideoDataOutput()
+            output.videoSettings = [
+                String(kCVPixelBufferPixelFormatTypeKey) : Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
+            ]
             
-            self.highlightView?.frame    = convertedRect
+            output.alwaysDiscardsLateVideoFrames = true
+            
+            if session.canAddOutput(output) {
+                session.addOutput(output)
+            }
+            
+            session.commitConfiguration()
+            let queue = DispatchQueue(label: "OutputQueue")
+            output.setSampleBufferDelegate(self, queue: queue)
+        } catch {
+            print("Cannot setup session")
         }
     }
-    
-    
-    @IBAction private func userTapped(_ sender: UITapGestureRecognizer) {
-        //get the tap center
-        self.highlightView?.frame.size  = CGSize(width: 120, height: 120)
-        self.highlightView?.center      = sender.location(in: self.view)
-        
-        //convert rect to initial observation
-        let originalRect        = self.highlightView?.frame ?? .zero
-        var convertedRect       = self.cameraLayer.metadataOutputRectConverted(fromLayerRect: originalRect)
-        convertedRect.origin.y  = 1 - convertedRect.origin.y
-        
-        //set the observation
-        let newObservation = VNDetectedObjectObservation(boundingBox: convertedRect)
-        self.lastObservation = newObservation
-    }
-
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard
-            //get CVPixelBuffer
-            let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
-            //make sure there is previous observation into request
-            let lastObservation = self.lastObservation
-            else { return }
         
-        //create request
-        let request = VNTrackObjectRequest(detectedObjectObservation: lastObservation, completionHandler: self.handleVisionRequestUpdate)
-        //set accuracy high
-        request.trackingLevel = .accurate
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
         
-        //perform request
-        do {
-            try self.visionSequenceHandler.perform([request], on: pixelBuffer)
-        } catch {
-            print("Throws: \(error.localizedDescription)")
-        }
+        let attachments = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate)
+        let ciImage = CIImage(cvImageBuffer: pixelBuffer!, options: attachments as! [String : Any]?)
+        
+        //leftMirrored for front camera
+        let ciImageWithOrientation = ciImage.oriented(forExifOrientation: Int32(UIImageOrientation.leftMirrored.rawValue))
+        
+        detectFace(on: ciImageWithOrientation)
     }
+    
 }
 
+extension ViewController {
+    
+    func detectFace(on image: CIImage) {
+        try? faceDetectionRequest.perform([faceDetection], on: image)
+        if let results = faceDetection.results as? [VNFaceObservation] {
+            if !results.isEmpty {
+                faceLandmarks.inputFaceObservations = results
+                detectLandmarks(on: image)
+                
+                DispatchQueue.main.async {
+                    self.shapeLayer.sublayers?.removeAll()
+                }
+            }
+        }
+    }
+    
+    func detectLandmarks(on image: CIImage) {
+        try? faceLandmarksDetectionRequest.perform([faceLandmarks], on: image)
+        if let landmarksResults = faceLandmarks.results as? [VNFaceObservation] {
+            for observation in landmarksResults {
+                DispatchQueue.main.async {
+                    if let boundingBox = self.faceLandmarks.inputFaceObservations?.first?.boundingBox {
+                        let faceBoundingBox = boundingBox.scaled(to: self.view.bounds.size) 
+                        
+                        //different types of landmarks
+                        let faceContour = observation.landmarks?.faceContour
+                        self.convertPointsForFace(faceContour, faceBoundingBox)
+                        
+                        //  mark left eye
+                        let leftEye = observation.landmarks?.leftEye
+                        self.convertPointsForFace(leftEye, faceBoundingBox)
+                        
+                        let leftPupil = observation.landmarks?.leftPupil
+                        self.convertPointsForFace(leftPupil, faceBoundingBox)
+                        
+                        // mark Right eye
+                        let rightEye = observation.landmarks?.rightEye
+                        self.convertPointsForFace(rightEye, faceBoundingBox)
+                        
+                        //  mark Noseeeeeee
+                        //  todo: nose capture seems off
+                        let nose = observation.landmarks?.nose
+                        self.convertPointsForFace(nose, faceBoundingBox)
+                        
+                        //  mark lips.inner
+                        //  todo: seem to be off
+                        let lips = observation.landmarks?.innerLips
+                        self.convertPointsForFace(lips, faceBoundingBox)
+                        
+                        //  mark alis kiri
+                        let leftEyebrow = observation.landmarks?.leftEyebrow
+                        self.convertPointsForFace(leftEyebrow, faceBoundingBox)
+                        
+                        //  mark alis kanan
+                        let rightEyebrow = observation.landmarks?.rightEyebrow
+                        self.convertPointsForFace(rightEyebrow, faceBoundingBox)
+                        
+                        //  mark nose crest
+                        //  todo: seems to be off
+                        let noseCrest = observation.landmarks?.noseCrest
+                        self.convertPointsForFace(noseCrest, faceBoundingBox)
+                        
+                        //  mark lips.outer
+                        let outerLips = observation.landmarks?.outerLips
+                        self.convertPointsForFace(outerLips, faceBoundingBox)
+                    }
+                }
+            }
+        }
+    }
+    
+    func convertPointsForFace(_ landmark: VNFaceLandmarkRegion2D?, _ boundingBox: CGRect) {
+        if let points = landmark?.normalizedPoints {
+            let faceLandmarkPoints = points.map { (point: CGPoint) -> (x: CGFloat, y: CGFloat) in
+                let pointX = point.x * boundingBox.width + boundingBox.origin.x
+                let pointY = point.y * boundingBox.height + boundingBox.origin.y
+                
+                return (x: pointX, y: pointY)
+            }
+            
+            DispatchQueue.main.async {
+                self.draw(points: faceLandmarkPoints)
+            }
+        }
+    }
+    
+    func draw(points: [(x: CGFloat, y: CGFloat)]) {
+        let newLayer = CAShapeLayer()
+        newLayer.strokeColor = UIColor.red.cgColor
+        newLayer.lineWidth = 2.0
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: points[0].x, y: points[0].y))
+        for i in 0..<points.count - 1 {
+            let point = CGPoint(x: points[i].x, y: points[i].y)
+            path.addLine(to: point)
+            path.move(to: point)
+        }
+        path.addLine(to: CGPoint(x: points[0].x, y: points[0].y))
+        newLayer.path = path.cgPath
+        
+        shapeLayer.addSublayer(newLayer)
+    }
+    
+    
+    func convert(_ points: UnsafePointer<vector_float2>, with count: Int) -> [(x: CGFloat, y: CGFloat)] {
+        var convertedPoints = [(x: CGFloat, y: CGFloat)]()
+        for i in 0...count {
+            convertedPoints.append((CGFloat(points[i].x), CGFloat(points[i].y)))
+        }
+        
+        return convertedPoints
+    }
+}
